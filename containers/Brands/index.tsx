e7247de97f27422517c773b2eb2bb387e7ad25a9
Frank Villanueva <frank05111996@gmail.com>
2022-05-26 19:37:20 -0500

    :construction: home

--- containers/Brands/index.tsx
+++ containers/Brands/index.tsx
@@ -2,41 +2,51 @@ import Image from "next/image";
 import styles from "./Brands.module.css";
 
 const Brands = () => {
+  const data = [
+    {
+      name: "Hyperx",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/hyperx.jpg",
+    },
+    {
+      name: "Logitech",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/logitech.jpg",
+    },
+    {
+      name: "Razer",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/razer.jpg",
+    },
+    {
+      name: "Nintendo",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/nintendo.jpg",
+    },
+    {
+      name: "Bose",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/bose.jpg",
+    },
+    {
+      name: "PS",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/ps.jpg",
+    },
+    {
+      name: "JBL",
+      image: "https://phantom.pe/pub/media/wysiwyg/marcas/jbl.jpg",
+    },
+  ];
   return (
     <div className={styles.Brands}>
       <h1 className={styles.BrandTitle}>Marcas</h1>
-      <div className={styles.BrandContainer}>
-        <div className={styles.BrandItem}>
-          <Image
-            src="https://w7.pngwing.com/pngs/110/633/png-transparent-laptop-asus-dell-logo-%E5%8D%8E%E7%A1%95-laptop-angle-electronics-text.png"
-            alt="Asus"
-            height={40}
-            width={180}
-          />
-        </div>
-        <div className={styles.BrandItem}>
-          <Image
-            src="https://logodownload.org/wp-content/uploads/2020/03/redragon-logo-2.png"
-            alt="Redragon"
-            height={40}
-            width={180}
-          />
-        </div>
-        <div className={styles.BrandItem}>
-          <Image
-            src="https://dtj8xnxpygn9a.cloudfront.net/wp-content/uploads/2020/12/TEROS-LOGO-01.png"
-            alt="Teros"
-            height={40}
-            width={180}
-          />
-        </div>
-        <div className={styles.BrandItem}>
-          <Image
-            src="https://e7.pngegg.com/pngimages/329/476/png-clipart-logo-gigabyte-technology-brand-gibibyte-general-electric-logo-blue-text.png"
-            alt="Gigabyte"
-            height={40}
-            width={180}
-          />
+      <div className={styles.BrandContainerWrapper}>
+        <div className={styles.BrandContainer}>
+          {data.map((item, index) => (
+            <div key={`item-brand-${index}`} className={styles.BrandItem}>
+              <Image
+                src={item.image}
+                alt={item.name}
+                width={180}
+                height={100}
+              />
+            </div>
+          ))}
         </div>
       </div>
     </div>
